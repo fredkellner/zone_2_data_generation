f34084af0628bdc35441c6df293e1629a7838350
Fred Kellner <34106393+fredkellner@users.noreply.github.com>
2025-10-22 06:47:59 -0600

    Update main.py
    updates maf function added in concat sets and changed name of index_stuff to something more descriptive

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ birthday = os.getenv('birthday')
 garmin = garminconnect.Garmin(email, password)
 garmin.login()
 
-def compute_maf_hr_on_birthdate(birth_date:str):
+def compute_maf_hr_on_birthdate(activity_date, birth_date:str):
   """Computes Zone 2 HR using Phil Maffetone's Zone 2 heart rate equation"""
   try:
     birth_date_obj = datetime.strptime(birth_date, "%Y-%m-%d")
   except ValueError:
     raise
-  years_old = int((datetime.datetime.today() - birth_date_obj).days / 365.25)
-  return = 180 - years_old
-
+  try:
+    activity_date_obj = datetime.strptime(activity_date, "%Y-%m-%d")
+  except ValueError:
+    raise
+  years_old = int((activity_date_obj  - birth_date_obj).days / 365.25)
+  return 180 - years_old
+ 
 def compute_acitivity_duration(activity):
    end_activity = int(activity['beginTimestamp'] + (activity['duration']*1000))
    duration = (
         datetime.datetime.fromtimestamp(end_activity/1000) 
-     - datetime.datetime.fromtimestamp(activity['beginTimestamp']/100).seconds  // 60 % 60
-  
-  
+     - datetime.datetime.fromtimestamp(activity['beginTimestamp']/100).seconds  // 60 % 60  
 
 temp_df_list = []
 for subtract_day in (range(0, datetime.datetime.today().timetuple().tm_yday)):
@@ -36,11 +38,7 @@ for subtract_day in (range(0, datetime.datetime.today().timetuple().tm_yday)):
   for activity in activities:
     activityID = str(activity['activityId'])
     activty_type = activity['activityType']['typeKey']
-    begin_activity = activity['beginTimestamp']
-    end_activity = int(activity['beginTimestamp'] + (activity['duration']*1000))
-    duration = (
-        datetime.datetime.fromtimestamp(end_activity/1000) -datetime.datetime.fromtimestamp(begin_activity/1000)
-        ).seconds  // 60 % 60
+    duration = compute_acitivity_duration(activity)
 
     # skip activities that are less than 10 minutes to
     # account for errors in starting watch. 
@@ -51,19 +49,25 @@ for subtract_day in (range(0, datetime.datetime.today().timetuple().tm_yday)):
       temp_hr_list = []
       timestamp_list = []
       activity = details.get('activityDetailMetrics')
-      index_stuff = details.get('metricDescriptors')
+      acitiviy_metrics = details.get('metricDescriptors')
       for metric in activity:
-        for val in index_stuff:
-          if val['key'] == 'directHeartRate':
-            hr_index = val['metricsIndex']
+        for metric in acitiviy_metrics:
+          if metric['key'] == 'directHeartRate':
+            hr_index = metric['metricsIndex']
             temp_hr_list.append(metric['metrics'][hr_index])
-          if val['key'] == 'directTimestamp':
-            time_index = val['metricsIndex']
+          if metric['key'] == 'directTimestamp':
+            time_index = metric['metricsIndex']
             timestamp_list.append(metric['metrics'][time_index])
       hr_val_df = pd.DataFrame(zip(timestamp_list, temp_hr_list), columns=['timestamp', 'hr'])
+      # add acitivty ID, duration start and end date and
+      # activity type to df
       hr_val_df['activityID'] =  activityID
       hr_val_df['duration_in_minutes'] = duration
       hr_val_df['date'] = start_date
       hr_val_df['activity'] = activty_type
-      temp_df_list.append( hr_val_df)
-      del( hr_val_df)
+      temp_df_list.append(hr_val_df)
+      del(hr_val_df)
+final_hr_df = pd.concat(temp_df_list)
+final_hr_df['date'] = pd.to_datetime(final_hr_df['date'])
+zone2_df = final_hr_df.query(f'hr <= 130').groupby(['activityID'])['hr'].count().reset_index().rename(columns={'hr': 'zone2'})
+final_hr_df['maf_hr'] = final_hr_df['date'].apply(lambda  x: compute_maf_hr_on_birthdate(x.strftime("%Y-%m-%d") , "1975-01-29"))
